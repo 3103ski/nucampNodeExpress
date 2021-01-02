083eb9b799d6817c2064a3ae4edc80a7b95cf728
3103ski <bjastski@gmail.com>
2021-01-02 11:24:03 -0800

    finishes week 1 workshop

--- server.js
+++ server.js
@@ -1,62 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
+const partnersRouter = require('./routes/partnersRouter');
+const promotionsRouter = require('./routes/promotionsRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.all('/campsites', (req, res, next) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	next();
-});
-
-// -------
-// Campsites
-// -------
-app.get('/campsites', (req, res) => {
-	res.end('Will return campsites data');
-});
-
-app.post('/campsites', (req, res) => {
-	res.end(
-		`Will add campsite: ${req.body.name} with description: ${req.body.description}`
-	);
-});
-
-app.put('/campsites', (req, res) => {
-	res.statusCode = 403;
-	res.end(`PUT operation is not supported`);
-});
-
-app.delete('/campsites', (req, res) => {
-	res.end('Deleting all campsites');
-});
-
-app.get('/campsites/:campsiteId', (req, res) => {
-	res.end(`Will send details about ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-	res.statusCode = 403;
-	res.end(
-		`POST operation not supported on /campsites/${req.params.campsiteId}`
-	);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-	res.write(`Updating campsite: ${req.params.campsiteId}\n`);
-	res.end(
-		`Will update the campsite: ${req.body.name} with description ${req.body.description}`
-	);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-	res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+app.use('/campsites', campsiteRouter);
+app.use('/partners', partnersRouter);
+app.use('/promotions', promotionsRouter);
 
 app.use(express.static(__dirname + '/public'));
 
